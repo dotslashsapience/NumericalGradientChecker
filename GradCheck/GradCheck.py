054c06a32b1f0d749c95f5ccb4d57083f00822ea
dotslashsapience <c.tristan94@outlook.com>
2025-11-05 21:46:21 -0800

    added comments for personal reference and added cost_minus pass.

--- GradCheck/GradCheck.py
+++ GradCheck/GradCheck.py
@@ -15,18 +15,24 @@ def compute_numerical_gradient(parameters, forward_prop_func, X, y, epsilon=1e-5
 
     Returns:
         numerical_grads -- dict, same structure as 'parameters', containing the numerical gradients for each param."""
-    numerical_grads = {}
-    for param_name, param_values in parameters.items():
-        numerical_grads[param_name] = np.zeros_like(param_values)
+    numerical_grads = {} #initialize dict
+    for param_name, param_values in parameters.items():#add entries to dict to match params
+        numerical_grads[param_name] = np.zeros_like(param_values)#initialize empty matrix values for keys and match dimensions.
 
-    for param_name, param_values in parameters.items():
+    for param_name, param_values in parameters.items():#iterate through dictionary
+        #get empty matrix from numerical_grads.
         num_grad_for_param = numerical_grads[param_name]
+        #get n-dimensional iterator for each matrix
         it = np.nditer(param_values, flags=['multi_index'], op_flags=[['readwrite']])
 
         while not it.finished:
-            idx = it.multi_index
-            original_value = param_values[idx]
-            param_values[idx] = original_value + epsilon
-
+            #iterate through each matrix.
+            idx = it.multi_index #iterator changes with each iteration of while loop.
+            original_value = param_values[idx] #store original value
+            param_values[idx] = original_value + epsilon #add epsilon and update parameter in matrix
+            #run a full forward pass with the updates parameter to get cost.
             cost_plus, _ = forward_prop_func(X, y, parameters)
+            param_values[idx] = original_value - epsilon #subtract eps and update parameter in matrix
+            #run forward pass with the cost minus epsilon
+            cost_minus, _ = forward_prop_func(X, y, parameters)
 
